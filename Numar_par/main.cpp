c0840ce881f1c398639397eafb5b95a23fc5fc5b
Ivanov Mihai <mihaigeorge.ivanov@gmail.com>
2023-12-18 23:23:04 +0200

    Determinare paritate numar

---
+++ Numar_par/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    while (true){
+       cout<<"n= ",cin>>n;
+        if(n%2==0)
+            cout<<"DA\n";
+
+        else
+            cout<<"NU\n";
+    }
+    return 0;
+}
